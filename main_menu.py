907f301d096589377a04f6386005bc9599030ab9
Crypenter <83304937+CrypenterTV@users.noreply.github.com>
2023-11-20 20:14:08 +0100

    menu graphique

--- main_menu.py
+++ main_menu.py
@@ -6,21 +6,25 @@ from gridparser import GridParser
 #Vert
 MENU_BACKGROUND = "#41B77F"
 
+def center_window(root : tk.Tk, window_width, window_height, percentage=10) -> None:
+        screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
+
+        x = (screen_width - window_width) // 2
+        y = (screen_height - window_height) // 2 - round(percentage/100 * (screen_height - window_height))
+
+        root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+
 class MainMenu:
 
     def __init__(self, root : tk.Tk) -> None:
 
         self.root = root
-        self.root.title("TAKUZU")
+        self.root.title("Jeu du Takuzu")
 
         window_width, window_height  = 500, 300
+        center_window(root, window_width, window_height)
 
-        screen_width, screen_height = root.winfo_screenwidth(), root.winfo_screenheight()
-
-        x = (screen_width - window_width) // 2
-        y = (screen_height - window_height) // 2 - round(10/100 * (screen_height - window_height))
-
-        self.root.geometry(f"{window_width}x{window_height}+{x}+{y}")
         self.root.resizable(False, False)
 
         self.root.config(background=MENU_BACKGROUND)
@@ -38,9 +42,12 @@ class MainMenu:
         file_button = tk.Button(root, text="Charger une grille depuis un fichier", font=("Courrier", 15, 'bold'), bg="white", fg="black", command=self.load_from_file)
         file_button.pack(pady=10)
 
-    def load_from_file(self):
+    def load_from_file(self) -> None:
+
         file_path = self.choose_file()
+
         if file_path:
+            
             self.root.destroy()
             r = tk.Tk()
             parser = GridParser(file_path)
@@ -52,13 +59,16 @@ class MainMenu:
             print(takuzu_solved)
             print(takuzu_solved.is_valid())
             tk_menu = TakuzuMenu(r, takuzu, takuzu_solved)
-
-
+            
+        
 
     def choose_file(self):
         file_path = filedialog.askopenfilename(title="Sélectionner un fichier", filetypes=[("Fichiers texte", "*.txt")])
 
         return file_path
+    
+    def get_root(self):
+        return self.root
 
     
 
--- takuzu_menu.py
+++ takuzu_menu.py
@@ -2,11 +2,9 @@ import tkinter as tk
 from tkinter import ttk, font
 from takuzu import Takuzu
 from gridparser import GridParser
+import main_menu
 
-
-locations = []
-
-def create_label_grid(grid, parent, solved=False):
+def create_label_grid(grid, parent):
     custom_font = font.nametofont("TkDefaultFont")
     custom_font.configure(size=20)
 
@@ -15,38 +13,38 @@ def create_label_grid(grid, parent, solved=False):
             text = grid[i][j]
             if text == -1:
                 text = '_'
-                locations.append((i,j))
-            if solved:
-                if (i, j) in locations:
-                    label = tk.Label(parent, text=f"{text}", width=4, height=2, borderwidth=1, relief="solid", font=custom_font, fg='red')
-                    label.grid(row=i, column=j)
-                else:
-                    label = tk.Label(parent, text=f"{text}", width=4, height=2, borderwidth=1, relief="solid", font=custom_font)
-                    label.grid(row=i, column=j)
-            else:  
-                label = tk.Label(parent, text=f"{text}", width=4, height=2, borderwidth=1, relief="solid", font=custom_font)
-                label.grid(row=i, column=j)
+            label = tk.Label(parent, text=f"{text}", width=4, height=2, borderwidth=1, relief="solid", font=custom_font)
+            label.grid(row=i, column=j)
 
 
 
 class TakuzuMenu:
 
     def __init__(self, root : tk.Tk, takuzu : Takuzu, takuzu_solved : Takuzu):
-        root.title("Takuzu")
 
-        table = tk.Frame(root)
+        self.root = root
+
+        self.root.title("Jeu du Takuzu")
+
+        #main_menu.center_window(self.root)
+        self.root.resizable(False, False)
+        table = tk.Frame(self.root)
         table.grid(row=0, column=0)
 
         custom_font = font.nametofont("TkDefaultFont")
         custom_font.configure(size=20)
 
 
-        unsolved_label_frame = tk.LabelFrame(table, text="Unsolved Grid")
+        unsolved_label_frame = tk.LabelFrame(table, text="Grille de départ")
         unsolved_label_frame.grid(row=0, column=0)
         create_label_grid(takuzu.get_grid(), unsolved_label_frame)
 
-        # Create labels for the solved grid
-        solved_label_frame = tk.LabelFrame(table, text="Solved Grid")
-        solved_label_frame.grid(row=0, column=1)
-        create_label_grid(takuzu_solved.get_grid(), solved_label_frame, solved=True)
+
+        #solved_label_frame = tk.LabelFrame(table, text="Grille Résolue")
+        #solved_label_frame.grid(row=0, column=1)
+        #create_label_grid(takuzu_solved.get_grid(), solved_label_frame)
+
+        root.update_idletasks()
+        main_menu.center_window(root, root.winfo_width(), root.winfo_height(), 25)
+        
 
